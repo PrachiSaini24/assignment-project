62ae82039cca2148a662be1786bcbfacf4820c2e
PrachiSaini24 <PrachiSaini55133@gmail.com>
2025-06-18 07:16:27 +0530

    add some animation into the main section

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //js file
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  const mainContent = document.getElementById("mainContent");
+
+  setTimeout(() => {
+    loader.style.display = "none";
+    mainContent.classList.add("show");
+  }, 1000); // simulate a loading delay
+});
